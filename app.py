7662cc8053d66426dc8b5f0d9e730403bf4706cc
Wallik2 <runpan4edu@gmail.com>
2022-01-10 19:36:52 +0700

    Update certificates

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+import os
+
 
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 @app.route("/home")
 def home():
     return render_template('home.html')
+
+@app.route("/certificate")
+def certificate():
+    certificates = os.listdir('static/certificate')
+    return render_template('certificate.html',certificates=certificates)
 
 if __name__ == "__main__":
     app.run(debug=True)
